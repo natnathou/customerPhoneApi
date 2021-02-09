cc3922d1c2ebd29e5278ebafcc19f3b4d744878b
Netanel <ne.zaffran@gmail.com>
2021-02-09 10:08:58 +0200

    add User Claims to the cookie

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using customerPhoneApi.Data;
 using customerPhoneApi.helpers;
 using customerPhoneApi.services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,55 +15,57 @@ using Microsoft.Extensions.Logging;
 
 namespace customerPhoneApi
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseNpgsql(Configuration.GetConnectionString("MyWebApiConnection")));
+            services.AddAutoMapper(typeof(Startup));
+            services.AddControllers();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+          .AddCookie();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseWhen(context => context.Request.Path.StartsWithSegments("/user"), appBuilder =>
+                           {
+                               appBuilder.UseMiddleware<JwtMiddleware>();
+
+                           });
+            app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                        });
+
+
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(x => x.UseNpgsql(Configuration.GetConnectionString("MyWebApiConnection")));
-      services.AddAutoMapper(typeof(Startup));
-      services.AddControllers();
-      services.AddScoped<IUserService, UserService>();
-      services.AddScoped<IAuthService, AuthService>();
-      services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
-      services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-    }
-
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      // app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseWhen(context => context.Request.Path.StartsWithSegments("/user"), appBuilder =>
-                     {
-                       appBuilder.UseMiddleware<JwtMiddleware>();
-
-                     });
-      app.UseEndpoints(endpoints =>
-                  {
-                    endpoints.MapControllers();
-                  });
-
-
-    }
-  }
 }
--- helpers/JwtMiddleware.cs
+++ helpers/JwtMiddleware.cs
@@ -6,69 +6,78 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using customerPhoneApi.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features.Authentication;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace customerPhoneApi.helpers
 {
 
-  public class JwtMiddleware
-  {
-    private readonly RequestDelegate _next;
-    private readonly IOptions<AppSettings> _appSettings;
-    public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
+    public class JwtMiddleware
     {
-      _appSettings = appSettings;
-      _next = next;
-    }
-
-    public async Task Invoke(HttpContext context, DataContext dataContext)
-    {
-
-      var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split("bearer ").Last();
-      if (token != null)
-      {
-        try
+        private readonly RequestDelegate _next;
+        private readonly IOptions<AppSettings> _appSettings;
+        public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
         {
-          attachUserToContext(context, token, dataContext);
+            _appSettings = appSettings;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, DataContext dataContext)
+        {
+
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split("bearer ").Last();
+            if (token != null)
+            {
+                try
+                {
+                    attachUserToContext(context, token, dataContext);
+
+                }
+                catch (Exception ex)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    System.Console.WriteLine(ex.Message);
+                    return;
+
+                }
+                await _next(context);
+
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+        }
+
+        private async void attachUserToContext(HttpContext context, string token, DataContext dataContext)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Value.Secret);
+
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            }, out SecurityToken validatedToken);
+            var jwtToken = (JwtSecurityToken)validatedToken;
+
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            identity.AddClaims(jwtToken.Claims);
+            var principal = new ClaimsPrincipal(identity);
+            context.User = principal;
+            await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = true });
 
         }
-        catch
-        {
-          context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-          return;
 
-        }
-        await _next(context);
-
-      }
-      else
-      {
-        context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-        return;
-      }
-    }
-
-    private void attachUserToContext(HttpContext context, string token, DataContext dataContext)
-    {
-      var tokenHandler = new JwtSecurityTokenHandler();
-      var key = Encoding.ASCII.GetBytes(_appSettings.Value.Secret);
-
-      tokenHandler.ValidateToken(token, new TokenValidationParameters
-      {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ClockSkew = TimeSpan.Zero
-      }, out SecurityToken validatedToken);
-      var jwtToken = (JwtSecurityToken)validatedToken;
-      var identity = new ClaimsIdentity(jwtToken.Claims);
-      context.User.AddIdentity(identity);
 
     }
-
-
-  }
-}
+};
